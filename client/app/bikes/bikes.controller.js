66a40732804939a475ec59dbd08124ded6a14acd
Toni Nopanen <Toni@Tonis-MacBook-Pro.local>
2016-04-12 22:36:18 +0300

    json part 3

--- client/app/bikes/bikes.controller.js
+++ client/app/bikes/bikes.controller.js
@@ -5,13 +5,42 @@ class BikesComponent {
   constructor($http, socket, $scope) {
     this.$http = $http;
     this.socket = socket;
-    this.$scope = $scope
+    this.$scope = $scope;
+    this.info = [];
+
   }
 
   $onInit() {
     this.$http.get('http://www.citibikenyc.com/stations/json').then(response => {
-      this.data = response.data;
+      this.data = response.data.stationBeanList;
+      //console.log(this.data[2].stationName);
+      var that = this;
+
+      this.data.forEach(function(item){
+        that.$http({
+          url: '/api/bikes',
+          method: 'POST',
+          data: { 'name' : item.stationName, 'availableDocks': item.availableDocks, 'totalDocks': item.totalDocks }
+        })
+        .then(function(response) {
+          // success
+          console.log(response);
+          that.info.push(response.data);
+          //console.log(response.data);
+        },
+        function(response) { // optional
+          // failed
+          console.log(response);
+        });
+        });
+
+
+
     });
+  }
+
+  showJSON(id){
+    window.location = '/api/bikes/' + id;
   }
 }
 
